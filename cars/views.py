1d2fb03a0e6ee2c7d4cc300a5af4f1f47121f09e
pooja139 <poojatehlan1993@gmail.com>
2021-06-08 09:42:26 +0530

    completed search functionalities and admin customization

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import car
 
 # Create your views here.
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = car.objects.order_by('-created_date')
+    data = {
+        'cars': cars,
+    }
+    return render(request, 'cars/cars.html',data)
+
+def car_detail(request,id):
+    single_car = get_object_or_404(car, pk=id)
+    #get primary key , get data from datebase, if data not available then return 404
+
+    data = {
+        'single_car' : single_car,
+    }
+    return render(request,'cars/car_detail.html',data)
+
+def search(request):
+    cars =car.objects.order_by('-created_date')
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__iexact=keyword)
+
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            cars = cars.filter(model__iexact=model)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            cars = cars.filter(City__iexact=city)
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(year__iexact=year)
+
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            cars = cars.filter(body_style__iexact=body_style)
+
+    if 'min_price' in request.GET:
+        min_price =request.GET['min_price']
+        max_price =request.GET['max_price']
+        if max_price:
+            cars= cars.filter(price_gte=min_price, price_lte=max_price)
+
+    data={
+        'cars' : cars,
+    }
+    return render(request,'cars/search.html',data)
--- pages/views.py
+++ pages/views.py
@@ -7,10 +7,20 @@ def home(request):
     teams = Team.objects.all()
     featured_cars = car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = car.objects.order_by('-created_date')
+    #search_fields =car.objects.values('model','city','year','body_style')
+    model_search =car.objects.values_list('model', flat=True).distinct()
+    city_search =car.objects.values_list('city', flat=True).distinct()
+    year_search =car.objects.values_list('year', flat=True).distinct()
+    body_style_search =car.objects.values_list('body_style', flat=True).distinct()
     data = {
         'teams' : teams,
         'featured_cars' : featured_cars,
         'all_cars' : all_cars,
+        #'search_fields' :search_fields,
+        'model_search' :model_search,
+        'city_search' :city_search,
+        'year_search' : year_search,
+
     }
     return render(request,'pages/home.html',data)
 
